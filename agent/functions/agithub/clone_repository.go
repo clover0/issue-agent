4c968ac1ad2decb821422b4812b6dca21bfeed2f
takeshi.koenuma <t.koenuma2@gmail.com>
2025-02-23 22:26:51 +0900

    replace git command execution to go-git library

--- agent/functions/agithub/clone_repository.go
+++ agent/functions/agithub/clone_repository.go
@@ -3,7 +3,8 @@ package agithub
 import (
 	"fmt"
 	"os"
-	"os/exec"
+
+	"github.com/go-git/go-git/v5"
 
 	"github.com/clover0/issue-agent/config"
 	"github.com/clover0/issue-agent/logger"
@@ -15,17 +16,16 @@ func CloneRepository(lo logger.Logger, conf config.Config, workRepository string
 		lo.Error("GITHUB_TOKEN is not set")
 		return fmt.Errorf("GITHUB_TOKEN is not set\n")
 	}
-	lo.Info("cloning repository...\n")
-	cmd := exec.Command("git", "clone", "--depth", "1",
-		fmt.Sprintf("https://oauth2:%s@github.com/%s/%s.git", token, conf.Agent.GitHub.Owner, workRepository),
-		conf.WorkDir,
-	)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		lo.Error(string(output))
-		return fmt.Errorf("failed to clone repository: %w\n", err)
-	}
 
+	lo.Info("cloning repository...\n")
+	if _, err := git.PlainClone(conf.WorkDir, false, &git.CloneOptions{
+		URL: fmt.Sprintf("https://oauth2:%s@github.com/%s/%s.git",
+			token, conf.Agent.GitHub.Owner, workRepository),
+		Depth: 1,
+	}); err != nil {
+		return err
+	}
 	lo.Info("cloned repository successfully\n")
+
 	return nil
 }
--- agent/release.Dockerfile
+++ agent/release.Dockerfile
@@ -1,8 +1,4 @@
-FROM debian:bookworm-slim@sha256:40b107342c492725bc7aacbe93a49945445191ae364184a6d24fedb28172f6f7 AS release
-
-RUN apt-get update && apt-get install -y git \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+FROM gcr.io/distroless/static-debian12
 
 # Use binaly built by goreleaser
 COPY agent /usr/local/bin/agent
