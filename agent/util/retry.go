cd57af8a7e3bf88f78828ffdc750e7c8b403eb76
takeshi.koenuma <t.koenuma2@gmail.com>
2025-02-25 21:11:47 +0900

    retry after waiting 60 seconds on Anthropic rate limiting

--- agent/util/retry.go
+++ agent/util/retry.go
@@ -5,12 +5,19 @@ import (
 	"time"
 )
 
-var RetryableError = errors.New("retryable error")
+var (
+	RetryableError          = errors.New("retryable error")
+	RetryAfter60SecondError = errors.New("retry after 60 seconds error")
+)
 
 func Retry(maxRetry int, f func() error) error {
 	for i := 0; i < maxRetry; i++ {
 		time.Sleep(time.Duration(i*i) * time.Second)
 		if err := f(); err != nil {
+			if errors.Is(err, RetryAfter60SecondError) {
+				time.Sleep(60 * time.Second)
+				continue
+			}
 			if errors.Is(err, RetryableError) {
 				continue
 			}
