ccb5a88fdc8e0c5e0efeee1e71d166aba1c196ef
Koenuma <clover0@users.noreply.github.com>
2025-02-28 09:00:34 +0900

    improve list-files function to list recursively (#144)

--- agent/functions/list_files.go
+++ agent/functions/list_files.go
@@ -3,21 +3,31 @@ package functions
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const FuncListFiles = "list_files"
 
 func InitListFilesFunction() Function {
 	f := Function{
-		Name:        FuncListFiles,
-		Description: "List the files within the directory like Unix ls command. Each line contains the file mode, byte size, and name",
-		Func:        ListFiles,
+		Name: FuncListFiles,
+		Description: strings.ReplaceAll(`List the files within the direc tory like Unix ls command.
+Each line contains the file mode, byte size, and name. If you want to list subdirectories recursively, use the depth option.`, "\n", ""),
+		Func: ListFiles,
 		Parameters: map[string]interface{}{
 			"type": "object",
 			"properties": map[string]interface{}{
 				"path": map[string]interface{}{
 					"type":        "string",
 					"description": "The valid path to list within its directory",
+				},
+				"depth": map[string]interface{}{
+					"type":        "number",
+					"description": "The depth of the directory to list subdirectory recursively. Default is 2",
+					"minimum":     1,
+					"default":     2,
+					"maximum":     3,
 				},
 			},
 			"required":             []string{"path"},
@@ -31,7 +41,8 @@ func InitListFilesFunction() Function {
 }
 
 type ListFilesInput struct {
-	Path string
+	Path  string
+	Depth int
 }
 
 func ListFiles(input ListFilesInput) ([]string, error) {
@@ -43,9 +54,17 @@ func ListFiles(input ListFilesInput) ([]string, error) {
 		return nil, fmt.Errorf("%s does not exist: %w", input.Path, err)
 	}
 
-	entries, err := os.ReadDir(input.Path)
+	if input.Depth <= 0 {
+		input.Depth = 2
+	}
+
+	return listFilesRecursive(input.Path, 1, input.Depth, "")
+}
+
+func listFilesRecursive(path string, currentDepth, maxDepth int, prefix string) ([]string, error) {
+	entries, err := os.ReadDir(path)
 	if err != nil {
-		return nil, fmt.Errorf("can't read directory at %sr: %w", input.Path, err)
+		return nil, fmt.Errorf("can't read directory at %s: %w", path, err)
 	}
 
 	var files []string
@@ -55,9 +74,27 @@ func ListFiles(input ListFilesInput) ([]string, error) {
 			return nil, fmt.Errorf("get file info error: %w", err)
 		}
 
+		fullName := entry.Name()
+		if prefix != "" {
+			fullName = prefix + "/" + entry.Name()
+		}
+
 		files = append(files,
-			fmt.Sprintf("%s %d %s", info.Mode(), info.Size(), entry.Name()),
+			fmt.Sprintf("%s %d %s", info.Mode(), info.Size(), fullName),
 		)
+
+		if entry.IsDir() && currentDepth < maxDepth {
+			subFiles, err := listFilesRecursive(
+				filepath.Join(path, entry.Name()),
+				currentDepth+1,
+				maxDepth,
+				fullName,
+			)
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, subFiles...)
+		}
 	}
 
 	return files, nil
