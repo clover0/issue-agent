32a974de292a9b073083753327f7dc03fb627d2e
takeshi.koenuma <t.koenuma2@gmail.com>
2025-03-01 14:33:26 +0900

    tools

---
+++ agent/agent/tools.go
@@ -0,0 +1,16 @@
+package agent
+
+import (
+	"github.com/clover0/issue-agent/functions"
+)
+
+func PlanTools() []functions.Function {
+	m := functions.FunctionsMap()
+
+	return []functions.Function{
+		m[functions.FuncOpenFile],
+		m[functions.FuncListFiles],
+		m[functions.FuncSearchFiles],
+		m[functions.FuncGetPullRequest],
+	}
+}
