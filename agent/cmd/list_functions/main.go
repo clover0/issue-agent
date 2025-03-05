9906f72454bf3f07b9bd9539309ae8264cea724b
takeshi.koenuma <t.koenuma2@gmail.com>
2025-03-06 08:37:20 +0900

    relocate agithub package

--- agent/cmd/list_functions/main.go
+++ agent/cmd/list_functions/main.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/google/go-github/v69/github"
 
+	"github.com/clover0/issue-agent/agithub"
 	"github.com/clover0/issue-agent/config"
 	"github.com/clover0/issue-agent/functions"
-	"github.com/clover0/issue-agent/functions/agithub"
 	"github.com/clover0/issue-agent/logger"
 )
 
