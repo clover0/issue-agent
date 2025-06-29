3994e605138286275284eba5670777b4036e2d2f
takeshi.koenuma <t.koenuma2@gmail.com>
2025-06-30 08:45:17 +0900

    rename

--- agent/core/orchestrator.go
+++ agent/core/orchestrator.go
@@ -185,10 +185,10 @@ func OrchestrateAgentsByComment(
 			parameter,
 			lo,
 			llmForwarder,
-			ReactTools(),
+			CommentingTools(),
 		))
 
-	tools := slices.Concat(ReactTools(), InvokeAgentTools())
+	tools := slices.Concat(CommentingTools(), InvokeAgentTools())
 
 	lo.Info("allowed functions: %s\n", strings.Join(util.Map(
 		tools,
